b9ad378305f4c60ff237ddfab4be638396271e6d
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-06-17 21:08:32 +0600

    Instant passing added

---
+++ app/src/main/java/com/example/intenetpassing/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.intenetpassing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button nextBtn;
+        nextBtn = findViewById(R.id.nextBtn);
+        Intent iNext;
+        iNext = new Intent(MainActivity.this, SecondActivity.class);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(iNext);
+            }
+        });
+    }
+}
